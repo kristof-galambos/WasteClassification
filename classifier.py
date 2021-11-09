cfe9dfc1f472bbc0bca4f492a538a596d804e108
csanad-kovacshazi <kovi.csani@gmail.com>
2021-11-09 22:00:34 +0100

    valami

--- classifier.py
+++ classifier.py
@@ -16,6 +16,10 @@ for i, filepath in enumerate(os.listdir(basepath)):
     if i==999:
         break
 
+
+#legyen egy lista train_features(képek), train_labels(0 ha O,1 ha R), test_features (képek)
+#meg kell keverni, hogy ne egymás után jöjjenek az O és R képek
+
 cv2.imshow('image', train[500])
 
 # # use the neural net for training and prediction
