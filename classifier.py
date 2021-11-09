5c03095e78f08220afe8d2d480c4a6e2e7043564
csanad-kovacshazi <kovi.csani@gmail.com>
2021-11-09 22:59:13 +0100

    valami

--- classifier.py
+++ classifier.py
@@ -37,19 +37,20 @@ def create_train_labels():
     for i in range(0, 1000):
         train_labels.append(1)
 
-def create_2_random_list():
+def create_2_random_list(train_features, train_labels):
     c = list(zip(train_features, train_labels))
     random.shuffle(c)
     train_features, train_labels = zip(*c)
+    return train_features, train_labels
 
 
 
 append_1000_o()
 append_1000_r()
 create_train_labels()
-create_2_random_list()
-print(train_labels)
-print(train_features)
+train_features_shuffled, train_labels_shuffled = create_2_random_list(train_features,train_labels)
+cv2.imshow('anyad', train_features_shuffled[100])
+print(train_labels_shuffled[100])
 
 #legyen egy lista train_features(képek), train_labels(0 ha O,1 ha R), test_features (képek)
 #meg kell keverni, hogy ne egymás után jöjjenek az O és R képek
