7c0face9f609a090ed1a3dc6104f0db772c89bcc
Kristof Galambos <kris.galambos@gmail.com>
2021-11-11 17:54:47 +0100

    refactored feature creation

--- classifier.py
+++ classifier.py
@@ -1,7 +1,7 @@
 
 import cv2
 import os
-#from neuralnet import NeuralNet
+from neuralnet import NeuralNet
 import random
 
 # read in and prepare the data
@@ -14,26 +14,26 @@ basepath_train_r = 'DATASET/TRAIN/R'
 basepath_test_o = 'DATASET/TEST/O'
 basepath_test_r = 'DATASET/TEST/R'
 
-dim = (185, 250)
+# dim = (185, 250)
+dim = (18, 25)
 
 
-#append 1000 train O pictures
-def train_append_1000_o():
-    for i, filepath in enumerate(os.listdir(basepath_train_o)):
-        img = cv2.imread(basepath_train_o + '/' + filepath)
+def read_pictures(basepath):
+    dataset = []
+    for i, filepath in enumerate(os.listdir(basepath)):
+        img = cv2.imread(basepath + '/' + filepath)
         img_resized = cv2.resize(img, dim)
-        train_features.append(img_resized)
+        dataset.append(img_resized)
         if i == 999:
             break
+    return dataset
 
-#append 1000 R pictures
-def train_append_1000_r():
-    for i, filepath in enumerate(os.listdir(basepath_train_r)):
-        img = cv2.imread(basepath_train_r + '/' + filepath)
-        img_resized = cv2.resize(img, dim)
-        train_features.append(img_resized)
-        if i == 999:
-            break
+train_features_o = read_pictures(basepath_train_o)
+train_features_r = read_pictures(basepath_train_r)
+test_features_o = read_pictures(basepath_test_o)
+test_features_r = read_pictures(basepath_test_r)
+train_features = train_features_o + train_features_r
+test_features = test_features_o + test_features_r
 
 #create train labels with 1000 0-s and 1000 1-s
 def create_train_labels():
@@ -41,22 +41,6 @@ def create_train_labels():
         train_labels.append(0)
     for i in range(0, 1000):
         train_labels.append(1)
-
-def test_append_1000_o():
-    for i, filepath in enumerate(os.listdir(basepath_test_o)):
-        img = cv2.imread(basepath_test_o + '/' + filepath)
-        img_resized = cv2.resize(img, dim)
-        test_features.append(img_resized)
-        if i == 999:
-            break
-
-def test_append_1000_r():
-    for i, filepath in enumerate(os.listdir(basepath_test_r)):
-        img = cv2.imread(basepath_test_r + '/' + filepath)
-        img_resized = cv2.resize(img, dim)
-        train_features.append(img_resized)
-        if i == 999:
-            break
 
 def create_test_labels():
     for i in range(0, 1000):
@@ -77,27 +61,26 @@ def create_2_random_test_list(test_features, test_labels):
     return test_features, test_labels
 
 
-train_append_1000_o()
-train_append_1000_r()
-test_append_1000_r()
-test_append_1000_o()
 create_test_labels()
 create_train_labels()
 train_features_shuffled, train_labels_shuffled = create_2_random_train_list(train_features, train_labels)
 test_features_shuffled, test_labels_shuffled = create_2_random_test_list(test_features, test_labels)
 
 # to test if code works
-cv2.imshow('anyad', train_features_shuffled[100])
+cv2.imshow('my test image', train_features_shuffled[100])
 print(train_labels_shuffled[100])
 
-cv2.imshow('anyad', test_features_shuffled[100])
+cv2.imshow('my test image', test_features_shuffled[100])
 print(test_labels_shuffled[100])
 
 #legyen egy lista train_features(képek), train_labels(0 ha O,1 ha R), test_features (képek)
 #meg kell keverni, hogy ne egymás után jöjjenek az O és R képek
 #n x width x 3 legyen az összes kép
 
-# # use the neural net for training and prediction
-# nn = NeuralNet()
-# nn.train(train_features, train_labels)
-# predicted_labels = nn.predict(test_features)
+# use the neural net for training and prediction
+nn = NeuralNet()
+nn.train(train_features, train_labels)
+predicted_labels = nn.predict(test_features)
+print('The accuracy score is: {:.4f}'.format(nn.get_accuracy_score(test_labels)))
+print('The confusion matrix is:')
+nn.print_confusion_matrix(test_labels)
--- digits.py
+++ digits.py
@@ -6,42 +6,42 @@ from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 from neuralnet import NeuralNet, ConvNet
 
-################ getting the smaller dataset ##############################
-# digits = load_digits()
-# X = digits.data.reshape(-1, 8, 8)
-# y = digits.target
+############### getting the smaller dataset ##############################
+digits = load_digits()
+X = digits.data.reshape(-1, 8, 8)
+y = digits.target
 
-# X = np.array([X[i] for i in range(len(X)) if y[i] == 0 or y[i] == 1])
-# y = np.array([y[i] for i in range(len(y)) if y[i] == 0 or y[i] == 1])
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
-# # y_train = to_categorical(y_train)
-# print(X_train.shape)
-# print(y_train.shape)
+X = np.array([X[i] for i in range(len(X)) if y[i] == 0 or y[i] == 1])
+y = np.array([y[i] for i in range(len(y)) if y[i] == 0 or y[i] == 1])
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+# y_train = to_categorical(y_train)
+print(X_train.shape)
+print(y_train.shape)
 
-# nn = NeuralNet()
-# nn.train(X_train, y_train)
-# y_pred = nn.predict(X_test)
-# print('The accuracy score is: {:.4f}'.format(nn.get_accuracy_score(y_test)))
-# print('The confusion matrix is:')
-# nn.print_confusion_matrix(y_test)
-
-
-############################## getting the larger dataset ######################
-first_class = 8
-second_class = 4
-(X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
-X_train = np.array([X_train[i] for i in range(len(X_train)) if y_train[i] == first_class or y_train[i] == second_class])
-y_train = np.array([y_train[i] for i in range(len(y_train)) if y_train[i] == first_class or y_train[i] == second_class])
-X_test = np.array([X_test[i] for i in range(len(X_test)) if y_test[i] == first_class or y_test[i] == second_class])
-y_test = np.array([y_test[i] for i in range(len(y_test)) if y_test[i] == first_class or y_test[i] == second_class])
-y_train[y_train == first_class] = 0
-y_train[y_train == second_class] = 1
-y_test[y_test == first_class] = 0
-y_test[y_test == second_class] = 1
-
-cnn = ConvNet()
-cnn.train(X_train, y_train)
-y_pred = cnn.predict(X_test)
-print('The accuracy score is: {:.4f}'.format(cnn.get_accuracy_score(y_test)))
+nn = NeuralNet()
+nn.train(X_train, y_train)
+y_pred = nn.predict(X_test)
+print('The accuracy score is: {:.4f}'.format(nn.get_accuracy_score(y_test)))
 print('The confusion matrix is:')
-cnn.print_confusion_matrix(y_test)
+nn.print_confusion_matrix(y_test)
+
+
+# ############################## getting the larger dataset ######################
+# first_class = 8
+# second_class = 4
+# (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
+# X_train = np.array([X_train[i] for i in range(len(X_train)) if y_train[i] == first_class or y_train[i] == second_class])
+# y_train = np.array([y_train[i] for i in range(len(y_train)) if y_train[i] == first_class or y_train[i] == second_class])
+# X_test = np.array([X_test[i] for i in range(len(X_test)) if y_test[i] == first_class or y_test[i] == second_class])
+# y_test = np.array([y_test[i] for i in range(len(y_test)) if y_test[i] == first_class or y_test[i] == second_class])
+# y_train[y_train == first_class] = 0
+# y_train[y_train == second_class] = 1
+# y_test[y_test == first_class] = 0
+# y_test[y_test == second_class] = 1
+
+# cnn = ConvNet()
+# cnn.train(X_train, y_train)
+# y_pred = cnn.predict(X_test)
+# print('The accuracy score is: {:.4f}'.format(cnn.get_accuracy_score(y_test)))
+# print('The confusion matrix is:')
+# cnn.print_confusion_matrix(y_test)
